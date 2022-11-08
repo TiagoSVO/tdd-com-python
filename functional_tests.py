63d2056c79d5fcf27da3df6794085d029f026937
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2022-11-08 08:27:52 -0300

    [UPDATE] Updated the assertion of the site to test

--- functional_tests.py
+++ functional_tests.py
@@ -3,5 +3,8 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+print(browser.title)
+
+assert 'The install worked successfully! Congratulations!' in browser.title
+
 
